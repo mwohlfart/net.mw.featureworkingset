70af775c30da61aae41cf1d34b9c4d7bc080e85c
Michael Wohlfart <michael.wohlfart@gmail.com>
2013-02-24 16:17:37 +0100

    Changed working set page to support selection of single feature project

--- net.mw.featureworkingset/src/net/mw/featureworkingset/FeatureWorkingSetPage.java
+++ net.mw.featureworkingset/src/net/mw/featureworkingset/FeatureWorkingSetPage.java
@@ -4,36 +4,57 @@
 package net.mw.featureworkingset;
 
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
-import net.mw.featureworkingset.internal.jdt.AbstractWorkingSetWizardPage;
 import net.mw.featureworkingset.internal.jdt.JdtUtil;
 import net.mw.featureworkingset.internal.jdt.PdeUtil;
 
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.resources.IWorkspace;
 import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.core.runtime.Assert;
 import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IAdaptable;
 import org.eclipse.jdt.ui.JavaElementComparator;
+import org.eclipse.jface.dialogs.Dialog;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.viewers.ISelectionChangedListener;
+import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.jface.viewers.ITreeContentProvider;
-import org.eclipse.jface.viewers.TableViewer;
+import org.eclipse.jface.viewers.SelectionChangedEvent;
+import org.eclipse.jface.viewers.StructuredSelection;
 import org.eclipse.jface.viewers.TreeViewer;
 import org.eclipse.jface.viewers.Viewer;
+import org.eclipse.jface.viewers.ViewerFilter;
+import org.eclipse.jface.wizard.WizardPage;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Label;
 import org.eclipse.ui.IWorkingSet;
+import org.eclipse.ui.IWorkingSetManager;
+import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.dialogs.IWorkingSetPage;
 
 /**
  * @author Michael
  * 
  */
-public class FeatureWorkingSetPage extends AbstractWorkingSetWizardPage
-		implements IWorkingSetPage {
+public class FeatureWorkingSetPage extends WizardPage implements
+		IWorkingSetPage {
+	
+	private final class WorkingSetExistsFilter extends ViewerFilter {
 
-	public class WorkspaceFeatureProjectProvider implements
+		@Override
+		public boolean select(Viewer viewer, Object parentElement, Object element) {
+			// TODO: implement
+			return true;
+		}
+
+	}
+
+	private static class WorkspaceFeatureProjectProvider implements
 			ITreeContentProvider {
 
 		@Override
@@ -50,7 +71,7 @@ public class FeatureWorkingSetPage extends AbstractWorkingSetWizardPage
 		public Object[] getElements(Object inputElement) {
 
 			List<IProject> result = new ArrayList<IProject>();
-			
+
 			if (inputElement instanceof IWorkspace) {
 
 				IWorkspace workspace = (IWorkspace) inputElement;
@@ -68,7 +89,7 @@ public class FeatureWorkingSetPage extends AbstractWorkingSetWizardPage
 					}
 				}
 			}
-			
+
 			return result.toArray();
 		}
 
@@ -91,17 +112,16 @@ public class FeatureWorkingSetPage extends AbstractWorkingSetWizardPage
 
 	}
 
+	private TreeViewer fTree;
+
+	private IProject selectedFeatureProject;
+	private IWorkingSet fWorkingSet;
+
 	public FeatureWorkingSetPage() {
 		super("featureWorkingSetPage", "Feature Working Set", null);
 	}
 
-	@Override
-	protected String getPageId() {
-		return FeatureWorkingSetPlugin.FEATUREWORKINGSET_ID;
-	}
-
-	@Override
-	protected void configureTree(TreeViewer tree) {
+	private void configureTree(TreeViewer tree) {
 
 		tree.setLabelProvider(JdtUtil.getTreeLabelProvider());
 		tree.setComparator(new JavaElementComparator());
@@ -112,52 +132,114 @@ public class FeatureWorkingSetPage extends AbstractWorkingSetWizardPage
 	}
 
 	@Override
-	protected void configureTable(TableViewer table) {
-		table.setLabelProvider(JdtUtil.getTableLabelProvider());
+	public void createControl(Composite parent) {
+		initializeDialogUnits(parent);
+
+		Composite composite = new Composite(parent, SWT.NONE);
+		composite.setLayout(new GridLayout());
+		composite.setLayoutData(new GridData(GridData.HORIZONTAL_ALIGN_FILL));
+		setControl(composite);
+
+		Label label = new Label(composite, SWT.WRAP);
+		label.setText("Select Feature Project from Workspace:");
+		GridData gd = new GridData(GridData.GRAB_HORIZONTAL
+				| GridData.HORIZONTAL_ALIGN_FILL
+				| GridData.VERTICAL_ALIGN_CENTER);
+		label.setLayoutData(gd);
+
+		createTree(composite);
+
+		initializeSelectedElements();
+		validateInput();
+
+		if (selectedFeatureProject != null) {
+			fTree.setSelection(new StructuredSelection(selectedFeatureProject));
+		}
+		
+		fTree.refresh(true);
+		fTree.getTree().setFocus();
+
+		Dialog.applyDialogFont(composite);
+
 	}
 
-	@Override
-	protected Object[] getInitialWorkingSetElements(IWorkingSet workingSet) {
-
-		List<Object> result = new ArrayList<Object>();
-//		IAdaptable[] elements = workingSet.getElements();
-//
-//		for (IAdaptable adaptable : elements) {
-//			IProject project = (IProject) adaptable.getAdapter(IProject.class);
-//
-//			if (project != null
-//					&& FeatureWorkingSetUtil.isFeatureProject(project)) {
-//				result.add(adaptable);
-//			}
-//		}
-
-		return result.toArray(new Object[result.size()]);
+	private void initializeSelectedElements() {
+		if (fWorkingSet != null) {
+			String workingSetName = fWorkingSet.getName();
+			selectedFeatureProject = ResourcesPlugin.getWorkspace().getRoot()
+					.getProject(workingSetName);
+		}
 	}
 
-	@Override
-	protected Set<Object> getWorkingSetElements(Object[] selection) {
-		Set<Object> result = new HashSet<Object>();
+	private void createTree(Composite parent) {
 
-		try {
-			for (Object o : selection) {
-				if (o instanceof IProject) {
-					IProject project = (IProject) o;
+		fTree = new TreeViewer(parent, SWT.BORDER | SWT.H_SCROLL | SWT.V_SCROLL
+				| SWT.SINGLE);
+		fTree.getControl().setLayoutData(
+				new GridData(SWT.FILL, SWT.FILL, true, true));
 
-					if (PdeUtil.isFeatureProject(project)) {
-						result.add(project);
+		fTree.setFilters(new ViewerFilter[] { new WorkingSetExistsFilter() } );
+		fTree.setUseHashlookup(true);
 
-						IProject[] includedPluginProjects = FeatureWorkingSetUtil
-								.getIncludedPluginProjects(project);
-						result.addAll(Arrays.asList(includedPluginProjects));
-					}
-				}
+		configureTree(fTree);
+
+		fTree.addSelectionChangedListener(new ISelectionChangedListener() {
+			public void selectionChanged(SelectionChangedEvent event) {
+				ISelection selection = fTree.getSelection();
+				IStructuredSelection ssel = (IStructuredSelection) selection;
+				selectedFeatureProject = (IProject) ssel.getFirstElement();
 			}
-		} catch (CoreException e) {
-			FeatureWorkingSetPlugin.getDefault().getLog().log(e.getStatus());
-			return Collections.emptySet();
+		});
+	}
+
+	@Override
+	public void finish() {
+		if (fWorkingSet == null) {
+			IWorkingSetManager workingSetManager = PlatformUI.getWorkbench()
+					.getWorkingSetManager();
+
+			String workingSetName = selectedFeatureProject.getName();
+
+			fWorkingSet = workingSetManager.createWorkingSet(workingSetName,
+					new IAdaptable[] {});
+			fWorkingSet.setId(FeatureWorkingSetPlugin.FEATUREWORKINGSET_ID);
+		} else {
+			fWorkingSet.setName(selectedFeatureProject.getName());
 		}
 
-		return result;
+	}
+
+	@Override
+	public IWorkingSet getSelection() {
+		return fWorkingSet;
+	}
+
+	@Override
+	public void setSelection(IWorkingSet workingSet) {
+		Assert.isNotNull(workingSet, "Working set must not be null"); //$NON-NLS-1$
+		fWorkingSet = workingSet;
+		
+		initializeSelectedElements();
+		
+		if (getContainer() != null && getShell() != null) {
+			validateInput();
+		}
+	}
+
+	private void validateInput() {
+		String errorMessage = null;
+		String infoMessage = null;
+
+		if (!hasSelectedElement())
+			infoMessage = "Feature Project must be selected";
+
+		setMessage(infoMessage, INFORMATION);
+		setErrorMessage(errorMessage);
+		setPageComplete(errorMessage == null);
+	}
+
+	private boolean hasSelectedElement() {
+		return selectedFeatureProject != null;
 	}
 
 }
